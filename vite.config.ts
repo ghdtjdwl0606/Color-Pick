e920dffb5131e48d6d2b04dbca74670b7f3a146e
ghdtjdwl0606 <ghdtjdwl0606@gmail.com>
2026-01-17 02:05:52 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,11 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
   },
-  resolve: {
-    alias: {
-      // @를 src 폴더로 지정하는 것이 일반적입니다. 
-      // 현재 구조에 맞춰 사용하세요.
-      '@': path.resolve(__dirname, './src'), 
-    },
+ resolve: {
+  alias: {
+    '@': path.resolve(__dirname, './src'),
   },
+},
   // 빌드 시 정적 파일 경로 에러(404)를 방지하기 위해 추가
   base: '/', 
 });
