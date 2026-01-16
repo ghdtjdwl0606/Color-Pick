d0aea50bf0be170b583ee46d3c4d5fa9a13c10ae
ghdtjdwl0606 <ghdtjdwl0606@gmail.com>
2026-01-17 02:09:15 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,10 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
   },
- resolve: {
-  alias: {
-    '@': path.resolve(__dirname, './src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-},
-  // 빌드 시 정적 파일 경로 에러(404)를 방지하기 위해 추가
   base: '/', 
 });
